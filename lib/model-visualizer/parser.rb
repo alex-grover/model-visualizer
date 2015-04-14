e3babceda3eb5e90d7e07f54a50e3b8b3d05c592
Brandon Gottfried <bgott@stanford.edu>
2015-04-14 15:27:16 -0700

    fixed parsing bug

--- lib/model-visualizer/parser.rb
+++ lib/model-visualizer/parser.rb
@@ -4,7 +4,8 @@ require_relative 'model'
 
 class Parser
     def parse
-        files = Dir['app/models/*']
+        #TO DO make sure we're only grabbing model files -- not concerns dir
+        files = Dir['app/models/*.rb']
         models = Array.new
         curr_model = nil
 
@@ -13,30 +14,31 @@ class Parser
                 if line.include? 'class'
                     name = /class ([[:alpha:]]+)/.match(line)[1]
                     curr_model = Model.new(name)
-                end
-
-                if line.include? 'has_many'
+                    models.push(curr_model)
+                    #puts 'new model = ' + name
+                elsif line.include? 'has_many'
                     model_neighbor = /has_many :(\w+)/.match(line)[1]
                     curr_model.add_has_many(model_neighbor)
-                end
+                    #puts "\thas_many : " + model_neighbor
 
-                if line.include? 'has_one'
+                elsif line.include? 'has_one'
                     model_neighbor = /has_one :(\w+)/.match(line)[1]
                     curr_model.add_has_one(model_neighbor)
-                end
-
-                if line.include? 'belongs_to'
+                    #puts "\thas_one : " + model_neighbor
+                
+                elsif line.include? 'has_and_belongs_to_many'
+                    model_neighbor = /has_and_belongs_to_many :(\w+)/.match(line)[1]
+                    #puts "\thas_and_belongs_to : " + model_neighbor
+                    curr_model.add_has_and_belongs_to_many(model_neighbor)
+                
+                elsif line.include? 'belongs_to'
                     model_neighbor = /belongs_to :(\w+)/.match(line)[1]
                     curr_model.add_belongs_to(model_neighbor)
-                end
-
-                if line.include? 'has_and_belongs_to_many'
-                    model_neighbor = /has_and_belongs_to_many :(\w+)/.match(line)[1]
-                    curr_model.add_has_and_belongs_to_many(model_neighbor)
+                    #puts "\tbelongs_to : " + model_neighbor
                 end
             end
 
-            models.push(curr_model)
+
         end
 
         models
