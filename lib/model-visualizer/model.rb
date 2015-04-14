ef1bfc0c98a1a4b5facc0a6359d99355aa8684e2
Ryan Perez <rrperez@stanford.edu>
2015-04-14 16:08:05 -0700

    added schema parser functionality

--- lib/model-visualizer/model.rb
+++ lib/model-visualizer/model.rb
@@ -7,6 +7,9 @@ class Model
         @has_one = Array.new
         @has_many = Array.new
         @has_and_belongs_to_many = Array.new
+        @integer_attributes = Array.new
+        @string_attributes = Array.new
+        @foreign_keys = Array.new
     end
 
     def add_belongs_to(model)
@@ -25,12 +28,26 @@ class Model
     	@has_and_belongs_to_many << model
     end
 
+    def add_integer_attribute(integer)
+        @integer_attributes << integer
+    end
+
+    def add_string_attribute(string)
+        @string_attributes << string
+    end
+
+    def add_foreign_key_attribute(key)
+        @foreign_keys << key
+    end
+
     def to_s
         str = @name + ":"
         str += "\n\tbelongs_to: [" + @belongs_to.join(", ") + "]"
         str += "\n\thas_one: [" + @has_one.join(", ") + "]"
         str += "\n\thas_many: [" + @has_many.join(", ") + "]"
         str += "\n\thas_and_belongs_to_many: [" + @has_and_belongs_to_many.join(", ") + "]"
-        str
+        str += "\n\tinteger attributes: [" + @integer_attributes.join(", ") + "]"
+        str += "\n\tstring attributes: [" + @string_attributes.join(", ") + "]"
+        str += "\n\tforeign keys: [" + @foreign_keys.join(", ") + "]"
     end
 end
