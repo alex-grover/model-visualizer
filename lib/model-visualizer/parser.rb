e438325c785f4d3f58ef3abdc68e78efb91538b4
Alex Grover <ajgrover@stanford.edu>
2015-04-14 15:56:03 -0700

    Remove comments.

--- lib/model-visualizer/parser.rb
+++ lib/model-visualizer/parser.rb
@@ -4,8 +4,7 @@ require_relative 'model'
 
 class Parser
     def parse
-        #TO DO make sure we're only grabbing model files -- not concerns dir
-        files = Dir['app/models/*.rb']
+        files = Dir['app/models/*.rb'] # TODO make sure we're only grabbing model files -- not concerns dir
         models = Array.new
         curr_model = nil
 
@@ -15,30 +14,24 @@ class Parser
                     name = /class ([[:alpha:]]+)/.match(line)[1]
                     curr_model = Model.new(name)
                     models.push(curr_model)
-                    #puts 'new model = ' + name
+
                 elsif line.include? 'has_many'
                     model_neighbor = /has_many :(\w+)/.match(line)[1]
                     curr_model.add_has_many(model_neighbor)
-                    #puts "\thas_many : " + model_neighbor
 
                 elsif line.include? 'has_one'
                     model_neighbor = /has_one :(\w+)/.match(line)[1]
                     curr_model.add_has_one(model_neighbor)
-                    #puts "\thas_one : " + model_neighbor
-                
+
                 elsif line.include? 'has_and_belongs_to_many'
                     model_neighbor = /has_and_belongs_to_many :(\w+)/.match(line)[1]
-                    #puts "\thas_and_belongs_to : " + model_neighbor
                     curr_model.add_has_and_belongs_to_many(model_neighbor)
-                
+
                 elsif line.include? 'belongs_to'
                     model_neighbor = /belongs_to :(\w+)/.match(line)[1]
                     curr_model.add_belongs_to(model_neighbor)
-                    #puts "\tbelongs_to : " + model_neighbor
                 end
             end
-
-
         end
 
         models
