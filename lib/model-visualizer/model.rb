b95f798f0bba0da4af39e0f47b0f9e472da1c52e
Ryan Perez <rrperez@stanford.edu>
2015-04-20 17:43:47 -0700

    added all activerecord datatypes to schemaparser and model

--- lib/model-visualizer/model.rb
+++ lib/model-visualizer/model.rb
@@ -5,6 +5,19 @@ class Model
     attr_reader :has_one
     attr_reader :has_many
     attr_reader :has_and_belongs_to_many
+    attr_reader :integer_attributes
+    attr_reader :string_attributes
+    attr_reader :primary_key_attributes
+    attr_reader :text_attributes
+    attr_reader :float_attributes
+    attr_reader :decimal_attributes
+    attr_reader :datetime_attributes
+    attr_reader :timestamp_attributes
+    attr_reader :time_attributes
+    attr_reader :date_attributes
+    attr_reader :binary_attributes
+    attr_reader :boolean_attributes
+    attr_reader :foreign_keys
 
     def initialize(name)
         @name = name
@@ -14,6 +27,16 @@ class Model
         @has_and_belongs_to_many = Array.new
         @integer_attributes = Array.new
         @string_attributes = Array.new
+        @primary_key_attributes = Array.new
+        @text_attributes = Array.new
+        @float_attributes = Array.new
+        @decimal_attributes = Array.new
+        @datetime_attributes = Array.new
+        @timestamp_attributes = Array.new
+        @time_attributes = Array.new
+        @date_attributes = Array.new
+        @binary_attributes = Array.new
+        @boolean_attributes = Array.new
         @foreign_keys = Array.new
     end
 
@@ -41,6 +64,46 @@ class Model
         @string_attributes << string
     end
 
+    def add_primary_key_attribute(primary_key)
+        @primary_key_attributes << primary_key
+    end
+
+    def add_text_attribute(text)
+        @text_attributes << text
+    end
+
+    def add_float_attribute(float)
+        @float_attributes << float
+    end
+
+    def add_decimal_attribute(decimal)
+        @decimal_attributes << decimal
+    end
+
+    def add_datetime_attribute(datetime)
+        @datetime_attributes << datetime
+    end
+
+    def add_timestamp_attribute(timestamp)
+        @timestamp_attributes << timestamp
+    end
+
+    def add_time_attribute(time)
+        @time_attributes << time
+    end
+
+    def add_date_attribute(date)
+        @date_attributes << date
+    end
+
+    def add_binary_attribute(binary)
+        @binary_attributes << binary
+    end
+
+    def add_boolean_attribute(boolean)
+        @boolean_attributes << boolean
+    end
+
     def add_foreign_key_attribute(key)
         @foreign_keys << key
     end
@@ -53,6 +116,16 @@ class Model
         str += "\n\thas_and_belongs_to_many: [" + @has_and_belongs_to_many.join(", ") + "]"
         str += "\n\tinteger attributes: [" + @integer_attributes.join(", ") + "]"
         str += "\n\tstring attributes: [" + @string_attributes.join(", ") + "]"
+        str += "\n\tprimary key attributes: [" + @primary_key_attributes.join(", ") + "]"
+        str += "\n\ttext attributes: [" + @text_attributes.join(", ") + "]"
+        str += "\n\tfloat attributes: [" + @float_attributes.join(", ") + "]"
+        str += "\n\tdecimal attributes: [" + @decimal_attributes.join(", ") + "]"
+        str += "\n\tdatetime attributes: [" + @datetime_attributes.join(", ") + "]"
+        str += "\n\ttimestamp attributes: [" + @timestamp_attributes.join(", ") + "]"
+        str += "\n\ttime attributes: [" + @time_attributes.join(", ") + "]"
+        str += "\n\tdate attributes: [" + @date_attributes.join(", ") + "]"
+        str += "\n\tbinary attributes: [" + @binary_attributes.join(", ") + "]"
+        str += "\n\tboolean attributes: [" + @boolean_attributes.join(", ") + "]"
         str += "\n\tforeign keys: [" + @foreign_keys.join(", ") + "]"
         str
     end
