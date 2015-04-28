ebdd46c64d31d0ed8708a6c74da2aa73f4f8fc5a
Steven Longoria <Sxl1092@stanford.edu>
2015-04-28 06:29:43 +0000

    Fixed errors in model-parser.rb where regex wasn't working correctly.

--- lib/model-visualizer/model-parser.rb
+++ lib/model-visualizer/model-parser.rb
@@ -15,24 +15,25 @@ class ModelParser
         # TODO: maybe factor this out into a parent class?
         for file in files
             IO.foreach(file) do |line|
-                if line.include? 'class'
+                line.strip!
+                if line.start_with? 'class'
                     name = /class ([[:alpha:]]+)/.match(line)[1]
                     curr_model = Model.new(name)
                     @models[name] = curr_model
 
-                elsif line.include? 'has_many'
+                elsif line.start_with? 'has_many'
                     model_neighbor = /has_many :(\w+)/.match(line)[1]
                     curr_model.add_has_many(model_neighbor)
 
-                elsif line.include? 'has_one'
+                elsif line.start_with? 'has_one'
                     model_neighbor = /has_one :(\w+)/.match(line)[1]
                     curr_model.add_has_one(model_neighbor)
 
-                elsif line.include? 'has_and_belongs_to_many'
+                elsif line.start_with? 'has_and_belongs_to_many'
                     model_neighbor = /has_and_belongs_to_many :(\w+)/.match(line)[1]
                     curr_model.add_has_and_belongs_to_many(model_neighbor)
 
-                elsif line.include? 'belongs_to'
+                elsif line.start_with? 'belongs_to'
                     model_neighbor = /belongs_to :(\w+)/.match(line)[1]
                     curr_model.add_belongs_to(model_neighbor)
                 end
