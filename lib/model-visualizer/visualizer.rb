8b90f0dcd4a9672e016bc4ee75633a748e63a6b3
Alex Grover <ajgrover@stanford.edu>
2015-04-28 17:03:30 -0700

    Fix JS indentation, pass data to d3

--- lib/model-visualizer/visualizer.rb
+++ lib/model-visualizer/visualizer.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/env ruby
 
+require 'json'
+
 class Visualizer
     def initialize(models)
         @models = models
@@ -60,52 +62,56 @@ class Visualizer
 
         var color = d3.scale.category20();
 
-        var force = d3.layout.force()
-            .charge(-120)
-            .linkDistance(30)
-            .size([width, height]);
+        var force = d3.layout
+                      .force()
+                      .charge(-120)
+                      .linkDistance(30)
+                      .size([width, height]);
 
-        var svg = d3.select("body").append("svg")
-            .attr("width", width)
-            .attr("height", height);
+        var svg = d3.select("body")
+                    .append("svg")
+                    .attr("width", width)
+                    .attr("height", height);
 
-        var models = document.getElementById("models").innerHTML;
-        graph = JSON.parse(models);
-        force
-            .nodes(graph.nodes)
+        var models = document.getElementById("models").innerHTML,
+            graph = JSON.parse(models);
+
+        force.nodes(graph.nodes)
             .links(graph.links)
             .start();
 
         var link = svg.selectAll(".link")
-            .data(graph.links)
-          .enter().append("line")
-            .attr("class", "link")
-            .style("stroke-width", function(d) { return Math.sqrt(d.value); });
+                      .data(graph.links)
+                      .enter().append("line")
+                      .attr("class", "link")
+                      .style("stroke-width", function(d) { return Math.sqrt(d.value); });
 
         var node = svg.selectAll(".node")
-            .data(graph.nodes)
-          .enter().append("circle")
-            .attr("class", "node")
-            .attr("r", 5)
-            .style("fill", function(d) { return color(d.group); })
-            .call(force.drag);
+                      .data(graph.nodes)
+                      .enter().append("circle")
+                      .attr("class", "node")
+                      .attr("r", 5)
+                      .style("fill", function(d) { return color(d.group); })
+                      .call(force.drag);
 
         node.append("title")
             .text(function(d) { return d.name; });
 
         force.on("tick", function() {
-          link.attr("x1", function(d) { return d.source.x; })
-              .attr("y1", function(d) { return d.source.y; })
-              .attr("x2", function(d) { return d.target.x; })
-              .attr("y2", function(d) { return d.target.y; });
+            link.attr("x1", function(d) { return d.source.x; })
+                .attr("y1", function(d) { return d.source.y; })
+                .attr("x2", function(d) { return d.target.x; })
+                .attr("y2", function(d) { return d.target.y; });
 
-          node.attr("cx", function(d) { return d.x; })
-              .attr("cy", function(d) { return d.y; });
+            node.attr("cx", function(d) { return d.x; })
+                .attr("cy", function(d) { return d.y; });
         });
 
-        </script>
-        </body>
-        </html>'
+        var data = '
+        # TODO: actually do something with data in JS
+        fileHtml.puts JSON.generate(@models)
+        fileHtml.puts '; console.log(data);'
+        fileHtml.puts '</script></body></html>'
         fileHtml.close()
     end
 end
