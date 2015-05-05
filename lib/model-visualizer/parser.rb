3ae051716cafdaf147c60547940664b46c9395c9
Alex Grover <ajgrover@stanford.edu>
2015-05-05 14:54:48 -0700

    Allow path to be passed at the command line.
    Closes #10

--- lib/model-visualizer/parser.rb
+++ lib/model-visualizer/parser.rb
@@ -4,14 +4,14 @@ require_relative 'model-parser'
 require_relative 'schema-parser'
 
 class Parser
-    def self.parse
+    def self.parse(root)
         models = Hash.new
 
         model_parser = ModelParser.new(models)
-        model_parser.parse
+        model_parser.parse root
 
         schema_parser = SchemaParser.new(models)
-        schema_parser.parse
+        schema_parser.parse root
 
         models
     end
