fcd81ac99bb4061a5fd23b89b49f8687359ce8cb
Alex Grover <ajgrover@stanford.edu>
2015-06-02 15:09:11 -0700

    Fix error with title when path is not given

--- lib/model-visualizer.rb
+++ lib/model-visualizer.rb
@@ -13,7 +13,13 @@ class ModelVisualizer
 
         root = if ARGV.length == 1 then ARGV[0] else '.' end
 
-        models = Parser.parse root
-        Visualizer.new(models).create_visualization(File.basename root)
+        path = File.absolute_path root
+
+        unless Dir.exists? path
+            abort 'Error: Path given does not exist!'
+        end
+
+        models = Parser.parse path
+        Visualizer.new(models).create_visualization(File.basename path)
     end
 end
